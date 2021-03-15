9b6dd51175ca30587a5531659f45a82c6f88e532
YanNeryM <80663773+YanNeryM@users.noreply.github.com>
2021-03-15 01:38:23 -0300

    Update fazerpedido.js

--- fazerpedido.js
+++ fazerpedido.js
@@ -2,18 +2,8 @@ box_formatarProduto.style.visibility = "hidden";
 box_limparProduto.style.visibility = "hidden";
 box_montarProduto.style.visibility = "hidden";
 
-var txt_nome = document.querySelector("#nome").value;
-
-txt_nome = window.encodeURIComponent(txt_nome);
-
-document.querySelector("#quantProduto").addEventListener(onchange, function () {
+document.querySelector('#quantProduto').addEventListener(onchange, function () {
 	var quantProduto = document.querySelector("#quantProduto");
 	quantProduto = window.encodeURIComponent(quantProduto);
 	console.log(quantProduto);
-	return 0;
-	});
-  
-  
-function enviarMensagem() {
-	window.open("https://api.whatsapp.com/send?phone=5581999776899" + "&text=","_blank");
-	}
+});
