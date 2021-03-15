4de537bb5da0ec44f220195dd522d8c7d04a26bb
YanNeryM <80663773+YanNeryM@users.noreply.github.com>
2021-03-15 02:09:19 -0300

    Update fazerpedido.js

--- fazerpedido.js
+++ fazerpedido.js
@@ -3,6 +3,6 @@ box_limparProduto.style.visibility = "hidden";
 box_montarProduto.style.visibility = "hidden";
 
 function sQuantProduto () {
-	var quantProduto = document.querySelector("#cQuantProduto");
+	var quantProduto = document.querySelector("#cQuantProduto").value;
 	console.log(quantProduto);
 }
