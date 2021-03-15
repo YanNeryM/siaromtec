9c4e9a71ddbcfd80a16e6948a9fde266b8dd068b
YanNeryM <80663773+YanNeryM@users.noreply.github.com>
2021-03-15 02:30:23 -0300

    Update fazerpedido.js

--- fazerpedido.js
+++ fazerpedido.js
@@ -5,20 +5,21 @@ box_montarProduto.style.visibility = "hidden";
 function sServico () {
 	var servico = document.querySelector("#cServico").value;
 	var nServico = servico;
+	console.log(nServico);
 	switch (nServico) {
-		case 0:
+		case "0":
 			box_formatarProduto.style.visibility = "visible";
 			box_limparProduto.style.visibility = "hidden";
 			box_montarProduto.style.visibility = "hidden";
 		break;
 		
-		case 1:
+		case "1":
 			box_formatarProduto.style.visibility = "hidden";
 			box_limparProduto.style.visibility = "visible";
 			box_montarProduto.style.visibility = "hidden";
 		break;
 		
-		case 2:
+		case "2":
 			box_formatarProduto.style.visibility = "hidden";
 			box_limparProduto.style.visibility = "hidden";
 			box_montarProduto.style.visibility = "visible";
